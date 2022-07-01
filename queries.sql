f5869ace5252691b8c8f21384bf796cd9c478429
Addis de la Fuente <vicaenmayo@outlook.com>
2022-06-30 22:51:55 -0400

    733

--- queries.sql
+++ queries.sql
@@ -53,3 +53,41 @@ retirement_info.last_name,
 FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+-- Using aliases 
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name, 
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Joining departments and dept_manager tables using aliases
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no
+
+SELECT ri.emp_no,
+	ri.First_name,
+	ri.Last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
