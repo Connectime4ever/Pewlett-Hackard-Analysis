9053f0a60e7b4d84472f2a18b0a86750c84a1d67
Addis de la Fuente <vicaenmayo@outlook.com>
2022-07-02 23:52:56 -0400

    Deliverable 1 done

--- Employee_Challenge_starter_code.sql
+++ Employee_Challenge_starter_code.sql
@@ -1,10 +1,15 @@
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT COUNT(title) AS "count", title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "count" DESC;
